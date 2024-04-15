1048309265bdb9baf29619eb5939afe6fe94101d
HARDEEP THETHI <130187380+Hardeep-dc@users.noreply.github.com>
2024-04-15 13:44:56 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -23,13 +23,12 @@ variable "availability_zone" {
   type    = list(string)
   default = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
- 
+
+#dev environment configuration of vps and subnets 
 variable "vpc_cidr" {
   type    = string
   default = "10.100.0.0/16"
 }
- 
-
 
 # VPC Public Subnets
 variable "vpc_public_subnets" {
@@ -43,3 +42,12 @@ variable "vpc_private_subnets" {
   type        = list(string)
   default     = ["10.100.1.0/24", "10.100.2.0/24", "10.100.3.0/24"]
 }
+
+
+
+#for staging environment you can use the following values and comment the above for dev.
+
+#variable "vpc_cidr" {
+#  type    = string
+#  default = "10.200.0.0/16"
+#}
