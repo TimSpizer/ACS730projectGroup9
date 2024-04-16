a8d6eae736f080684157bf618722deabcd46166d
Prithvich97 <113849850+Prithvich97@users.noreply.github.com>
2024-04-16 19:03:45 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -65,3 +65,10 @@ variable "vpc_private_subnets" {
 #  type        = list(string)
 #  default     = ["10.200.1.0/24", "10.200.2.0/24", "10.200.3.0/24"]
 }
+
+#for dev environment you can use the following values and comment the above for dev.
+
+#variable "vpc_cidr" {
+#  type    = string
+#  default = "10.250.0.0/16"
+#}
